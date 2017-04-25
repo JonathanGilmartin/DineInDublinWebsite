8e3a8ea744bf608d1655923b67a0220457236705
JonathanGilmartin <jonathan.gilmartin1994@hotmail.co.uk>
2017-04-25 19:49:24 +0100

    Update js.js

--- js.js
+++ js.js
@@ -1,1 +1,16 @@
 
+(function($){
+  $(function(){
+
+   $(".button-collapse").sideNav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+	
+	// Show sideNav
+  $('.button-collapse').sideNav('show');
+  // Hide sideNav
+  $('.button-collapse').sideNav('hide');
+  // Destroy sideNav
+  $('.button-collapse').sideNav('destroy');
