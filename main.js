033032671b909bed0bce6c300835b6912433c001
JonathanGilmartin <jonathan.gilmartin1994@hotmail.co.uk>
2017-04-25 19:19:24 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-('.button-collapse').sideNav({
+$function('.button-collapse').sideNav({
       menuWidth: 300, // Default is 300
       edge: 'right', // Choose the horizontal origin
       closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
