2ae1c6a1f112fdbfc5fb0217d1dc24e2bb437f8e
JonathanGilmartin <jonathan.gilmartin1994@hotmail.co.uk>
2017-04-27 19:44:17 +0100

    Update js.js

--- js.js
+++ js.js
@@ -1,11 +1,16 @@
- $(".button-collapse").sideNav();
-        			});
-  	
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
  	
  	$('.button-collapse').sideNav({
       menuWidth: 300, // Default is 300
-       edge: 'right', // Choose the horizontal origin
+      edge: 'right', // Choose the horizontal origin
       closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
       draggable: true // Choose whether you can drag to open on touch screens
     }
   );
+
